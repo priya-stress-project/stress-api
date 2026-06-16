abf252ff106778cafce64da4b700a60d9990a9d6
priya-stress-project <venkateshpriya0812@gmail.com>
2026-06-16 22:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import joblib
 
 app = Flask(__name__)
 
+model = joblib.load("stress_model.pkl")
+
 @app.route("/")
 def home():
-    return "Stress API Working"
+    return "Stress API + Model Loaded Successfully"
 
 if __name__ == "__main__":
     app.run()
