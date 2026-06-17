3aaac6b9658a949f0dde9b52e62d4f19bf5b2fd8
priya-stress-project <venkateshpriya0812@gmail.com>
2026-06-17 22:16:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Stress Prediction API Running"
+
 @app.route("/predict", methods=["POST"])
 def predict():
 
@@ -5,3 +13,6 @@ def predict():
         "files": list(request.files.keys()),
         "form": list(request.form.keys())
     })
+
+if __name__ == "__main__":
+    app.run()
