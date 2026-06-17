22f3ea80982c7907aeb975d52fabb28376325fec
priya-stress-project <venkateshpriya0812@gmail.com>
2026-06-17 22:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,66 @@
 from flask import Flask, request, jsonify
+import joblib
+import librosa
+import numpy as np
+import os
 
 app = Flask(__name__)
+
+# Load Model
+model = joblib.load("stress_model.pkl")
+
+# Feature Extraction
+def extract_features(file_path):
+
+    audio, sample_rate = librosa.load(
+        file_path,
+        duration=3,
+        offset=0.5
+    )
+
+    mfccs = librosa.feature.mfcc(
+        y=audio,
+        sr=sample_rate,
+        n_mfcc=40
+    )
+
+    mfccs_scaled = np.mean(
+        mfccs.T,
+        axis=0
+    )
+
+    return mfccs_scaled
+
 
 @app.route("/")
 def home():
     return "Stress Prediction API Running"
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
 
+    if "file" not in request.files:
+        return jsonify({
+            "error": "No file uploaded"
+        })
+
+    audio_file = request.files["file"]
+
+    temp_file = "audio.wav"
+
+    audio_file.save(temp_file)
+
+    features = extract_features(temp_file)
+
+    prediction = model.predict([features])
+
+    os.remove(temp_file)
+
     return jsonify({
-        "files": list(request.files.keys()),
-        "form": list(request.form.keys())
+        "stress": str(prediction[0])
     })
+
 
 if __name__ == "__main__":
     app.run()
